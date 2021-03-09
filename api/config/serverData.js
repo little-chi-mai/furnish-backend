9ca572976355db2d47bb61a66ceb415f9f2cbace
Andre Anggono <andre.anggono@hotmail.com>
2021-03-09 18:12:15 +1100

    fixing prod bugs

---
+++ api/config/serverData.js
@@ -0,0 +1,5 @@
+// export const ROOT = "http://localhost:3001";
+// export const PRODUCTS = "http://localhost:3001/products";
+
+export const ROOT = "https://furnique.herokuapp.com";
+export const PRODUCTS = "https://furnique.herokuapp.com/products";
--- api/controllers/saleController.js
+++ api/controllers/saleController.js
@@ -1,14 +1,14 @@
 const productController = require("./productController");
+const { ROOT, PRODUCTS } = require("../config/serverData");
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const mongoose = require("mongoose");
 const productModel = require("../models/productModel");
 const Sale = mongoose.model("Sale");
 // const {validateCartItems} = require('use-shopping-cart/src/serverUtil');
-const axios = require('axios');
+const axios = require("axios");
 const { response } = require("express");
 // const User = mongoose.model("User");
-
 
 // exports.getCheckoutSession = (req, res) => {
 // const product = await productModel.findById(req.params.tourId)
@@ -57,48 +57,46 @@ exports.listSales = (req, res) => {
 		});
 };
 
-
 exports.createCheckoutSession = async (req, res, next) => {
 	try {
-
 		const cartItems = req.body;
 
-		const cartItemsArray = Object.keys(cartItems).map(key => cartItems[key]);
+		const cartItemsArray = Object.keys(cartItems).map((key) => cartItems[key]);
 
 		const validateCartItems = (inventory, cartDetails) => {
 			const validatedItems = [];
-			cartItemsArray.map(cartItem => {
+			cartItemsArray.map((cartItem) => {
 				const inventoryItem = inventory.find((product) => product.id === cartItem.id);
-				if (!inventoryItem) throw new Error(`Product ${product.id} not found!`)
+				if (!inventoryItem) throw new Error(`Product ${product.id} not found!`);
 				const item = {
-				name: inventoryItem.name,
-				amount: inventoryItem.price * 100,
-				currency: "AUD",
-				quantity: cartItem.quantity
-				}
-				if (inventoryItem.description) item.description = inventoryItem.description
-				if (inventoryItem.image) item.images = [inventoryItem.image]
-				validatedItems.push(item)
-			})
+					name: inventoryItem.name,
+					amount: inventoryItem.price * 100,
+					currency: "AUD",
+					quantity: cartItem.quantity
+				};
+				if (inventoryItem.description) item.description = inventoryItem.description;
+				if (inventoryItem.image) item.images = [inventoryItem.image];
+				validatedItems.push(item);
+			});
 			return validatedItems;
-		}
+		};
 
-		const products = (await productController.allProducts());
+		const products = await productController.allProducts();
 		const line_items = validateCartItems(products, cartItems);
 
 		const session = await stripe.checkout.sessions.create({
 			submit_type: "pay",
-			payment_method_types: ['card'],
-			success_url: `http://localhost:3001/`,
-			cancel_url: `http://localhost:3001/products`,
-			line_items,
+			payment_method_types: ["card"],
+			success_url: ROOT,
+			cancel_url: PRODUCTS,
+			line_items
 		});
 
 		res.status(200).json({
-			status: 'success',
+			status: "success",
 			session
-		})
+		});
 	} catch (error) {
-		res.status(500).json({statusCode: 500, message: error.message});
+		res.status(500).json({ statusCode: 500, message: error.message });
 	}
-}
+};
