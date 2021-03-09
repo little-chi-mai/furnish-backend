ed6485df1eaa78172a7e3b76cb18623f6871bb8b
Andre Anggono <andre.anggono@hotmail.com>
2021-03-09 23:27:45 +1100

    Added search function

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -3,9 +3,9 @@ const productController = require("../controllers/productController");
 module.exports = (app) => {
 	app.route("/products/q").get(productController.findProductBy);
 
-	app.route("/products")
-		.get(productController.listProducts)
-		.post(productController.createProduct);
+	app.route("/products").get(productController.listProducts).post(productController.createProduct);
 
 	app.route("/products/:productId").get(productController.findProduct).put(productController.updateProduct).delete(productController.deleteProduct);
+
+	app.route("/search").get(productController.searchProduct);
 };
